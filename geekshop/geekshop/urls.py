c0c5d7cbc118c483fb41a7d5c065bdc1013e2d18
Three-cats <ns_ilnur@mail.ru>
2022-04-07 23:50:01 +0300

    static

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -21,5 +21,6 @@ from mainapp.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products')
+    path('products/', products, name='products'),
+    # path('test/', test, name='test'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -11,3 +11,32 @@ def products(request):
     return render(request, 'mainapp/products.html')
 
 
+# def test(request):
+#
+#     context = {
+#         'title': 'geekshop',
+#         'header': 'Welcome',
+#         'user': 'Ilnur',
+#         'products': [
+#             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+#             {'name': 'Синяя куртка The North Face', 'price': 23725},
+#             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+#             {'name': 'Черный рюкзак Nike Heritage', 'price': 2340},
+#             {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 13590},
+#             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 2890}
+#         ],
+#         'promotion': True,
+#         'products_promotion': [
+#             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 50},
+#             {'name': 'Синяя куртка The North Face', 'price': 50},
+#             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 50},
+#             {'name': 'Черный рюкзак Nike Heritage', 'price': 50},
+#             {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 50},
+#             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 50}
+#         ],
+#     }
+#     return render(request, 'mainapp/test.html', context)
+
+
+
+
