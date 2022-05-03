3473e51c3cdb4b1438e090888df32542b56af669
Three-cats <ns_ilnur@mail.ru>
2022-05-03 13:08:19 +0300

    lesson_8 фильтрация, постраничный вывод, детализация

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,7 +1,9 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 import json
 
 # Create your views here.
+from django.views.generic import DetailView
 from mainapp.models import Product, ProductCategories
 
 
@@ -17,10 +19,29 @@ def load_json(title):
         return json.load(from_json)
 
 
-def products(request):
+def products(request, id_category=None, page=1):
+    if id_category:
+        products = Product.objects.filter(id=id_category)
+    else:
+        products = Product.objects.all()
+
+    pagination = Paginator(products, per_page=2)
+
+    try:
+        product_pagination = pagination.page(page)
+    except PageNotAnInteger:
+        product_pagination = pagination.page(1)
+    except EmptyPage:
+        product_pagination = pagination.page(pagination.num_pages)
+
     content = {
         'title': 'Geekshop - Каталог',
         'categories': ProductCategories.objects.all(),
-        'products': Product.objects.all()
+        'products': product_pagination
     }
     return render(request, 'mainapp/products.html', content)
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
