dd249f997c1a02e5feda68f10f7e95d226c08ddb
Three-cats <ns_ilnur@mail.ru>
2022-05-21 09:38:48 +0300

    lesson_2_4

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from ordersapp.views import OrderList, OrderCreate, OrderUpdate, OrderRead, OrderDelete, order_forming_complete, order_change_status
+from ordersapp.views import OrderList, OrderCreate, OrderUpdate, OrderRead, OrderDelete, order_forming_complete, order_change_status, get_product_price
 
 app_name = 'ordersapp'
 urlpatterns = [
@@ -9,6 +9,8 @@ urlpatterns = [
     path('update/<int:pk>/', OrderUpdate.as_view(), name='update'),
     path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
     path('read/<int:pk>/', OrderRead.as_view(), name='read'),
-    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
+    path('forming_complete/<int:pk>/',
+         order_forming_complete, name='forming_complete'),
     path('change_status/<int:pk>/', order_change_status, name='change_status'),
+    path('product/<int:pk>/price/', get_product_price, name='product_price'),
 ]
